fd1f665eb8400dc6ee94693397850790190ba26a
Jonathan Reifer <jonathanr@osrce.com>
2012-11-26 20:16:45 -0500

    Fixed SrAclPermissions.java. updated pom to hibernate 4 with hibernate.spatial

--- src/main/java/com/osrce/sitrep/domain/SrAclPermissions.java
+++ src/main/java/com/osrce/sitrep/domain/SrAclPermissions.java
@@ -1,5 +1,8 @@
 package com.osrce.sitrep.domain;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.roo.addon.dbre.RooDbManaged;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
@@ -10,4 +13,26 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooJpaActiveRecord(versionField = "", table = "sr_acl_permissions", schema = "public")
 @RooDbManaged(automaticallyDelete = true)
 public class SrAclPermissions {
+
+    @Column(name = "role_type", columnDefinition="Roletype")
+    @NotNull
+    private Roletype roleType;
+	
+    @Column(name = "permission_create", columnDefinition="Permittype")
+    @NotNull
+    private Permittype permissionCreate;
+    
+    @Column(name = "permission_read", columnDefinition="Permittype")
+    @NotNull
+    private Permittype permissionRead;
+    
+    @Column(name = "permission_update", columnDefinition="Permittype")
+    @NotNull
+    private Permittype permissionUpdate;
+    
+    @Column(name = "permission_delete", columnDefinition="Permittype")
+    @NotNull
+    private Permittype permissionDelete;
+    
+	
 }
